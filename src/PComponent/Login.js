6ac79d6b98d989e7e1a2e57d439fdaf659cbbee7
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2022-09-27 15:16:16 +0330

    bug fixed

--- src/PComponent/Login.js
+++ src/PComponent/Login.js
@@ -40,72 +40,74 @@ export default function Login(props) {
         </div>
       );
     } else {
-      <div className="loginForm">
-        <form method="POST" onSubmit={handleSubmit}>
-          <div className="loginInput">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              onChange={props.handleUserInfo}
-              required
-              placeholder="First Name"
-              type="text"
-              name="firstName"
-              value={props.userInfo.firstName}
-              id="firstName"
-            />
-          </div>
-          <div className="loginInput">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              onChange={props.handleUserInfo}
-              required
-              placeholder="Last Name"
-              type="text"
-              name="lastName"
-              value={props.userInfo.lastName}
-              id="lastName"
-            />
-          </div>
-          <div className="loginInput">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={props.handleUserInfo}
-              required
-              placeholder="Email Address"
-              type="text"
-              name="email"
-              value={props.userInfo.email}
-              id="email"
-            />
-          </div>
-          <div className="loginInput">
-            <label htmlFor="country">Country</label>
-            <input
-              onChange={props.handleUserInfo}
-              placeholder="Country"
-              type="text"
-              name="country"
-              value={props.userInfo.country}
-              id="country"
-            />
-          </div>
-          <div className="loginInput">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={props.handleUserInfo}
-              required
-              placeholder="password"
-              type="password"
-              name="password"
-              value={props.userInfo.password}
-              id="password"
-            />
-          </div>
-          <div className="loginInput">
-            <input className="loginBtn" type="submit" value="Submit" />
-          </div>
-        </form>
-      </div>;
+      return (
+        <div className="loginForm">
+          <form method="POST" onSubmit={handleSubmit}>
+            <div className="loginInput">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                onChange={props.handleUserInfo}
+                required
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+                value={props.userInfo.firstName}
+                id="firstName"
+              />
+            </div>
+            <div className="loginInput">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                onChange={props.handleUserInfo}
+                required
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                value={props.userInfo.lastName}
+                id="lastName"
+              />
+            </div>
+            <div className="loginInput">
+              <label htmlFor="email">Email</label>
+              <input
+                onChange={props.handleUserInfo}
+                required
+                placeholder="Email Address"
+                type="text"
+                name="email"
+                value={props.userInfo.email}
+                id="email"
+              />
+            </div>
+            <div className="loginInput">
+              <label htmlFor="country">Country</label>
+              <input
+                onChange={props.handleUserInfo}
+                placeholder="Country"
+                type="text"
+                name="country"
+                value={props.userInfo.country}
+                id="country"
+              />
+            </div>
+            <div className="loginInput">
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={props.handleUserInfo}
+                required
+                placeholder="password"
+                type="password"
+                name="password"
+                value={props.userInfo.password}
+                id="password"
+              />
+            </div>
+            <div className="loginInput">
+              <input className="loginBtn" type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      );
     }
   };
   return <Login />;
